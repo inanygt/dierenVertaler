fbba1d59762a74d2ab0a52d65442bd27e375a813
inan <inanygt@hotmail.com>
2024-04-28 17:24:28 +0200

    feat: Add test startswithvowel method

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-
+  let component: AppComponent;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
@@ -24,6 +24,12 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Dierenvertaler');
   });
 
+  it('should initialize translation_input as undefined', () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  const app = fixture.componentInstance;
+  expect(app.translation_input).toBeUndefined();
+  });
+
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
@@ -36,9 +42,14 @@ describe('AppComponent', () => {
   //  })
 
   it('should return true for a word starting with a vowel', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = new AppComponent;
     const word = 'apple';
     expect(app.startsWithVowel(word)).toBe(true);
+  })
+
+  it('should return false for a word starting with a consonant', () => {
+    const app = new AppComponent;
+    const word = 'baby';
+    expect(app.startsWithVowel(word)).toBe(false);
   })
 });
